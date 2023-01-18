fd55b8a4f5bf62f397fc1cc20aaedb779dbf93e5
Carlos <carlosperezfrozono@gmail.com>
2023-01-17 23:46:05 -0400

    AVANCES LOGICA MODULO USER

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,16 @@ import axios from "axios";
 import { Container, Row, Button, Form } from "react-bootstrap";
 import "./App.css";
 
-//PENDIENTE: POST; CREAR Y ELIMINAR
 // HACER FORM GRANDE PARA INSERTAR
 //PROBAR EL RESTO DE FUNCIONES DE LOS OTROS MODELOS
 function App() {
   const { data, loading, error } = useFetch("http://localhost:5000/user");
 
   const [name, setName] = useState("");
-  const [phone, setPhone] = useState("");
+  const [pass, setPass] = useState("");
+  const [mail, setMail] = useState("");
+  const [type, setType] = useState("");
+  const [num, setNum] = useState(0);
 
   const sendUser = () => {
     axios
@@ -22,11 +24,27 @@ function App() {
       .then((res) => console.log(res));
   };
 
+  const delUser = () => {
+    axios
+      .delete(`http://localhost:5000/user/${num}`)
+      .then((res) => console.log(res));
+  };
+
+  const actUser = () => {
+    axios
+      .put(`http://localhost:5000/user/${num}`, {
+        email: mail,
+        userName: name,
+        password: pass,
+        type: type,
+      })
+      .then((res) => console.log(res));
+  };
+
   const onFormSubmit = (e) => {
     e.preventDefault();
-    console.log(name);
-    console.log(phone);
-    sendUser();
+    console.log(name, pass, mail, type);
+    actUser();
   };
 
   if (loading) return <h1>CARGANDO</h1>;
@@ -39,7 +57,7 @@ function App() {
 
           <Row className="md">
             <Form onSubmit={onFormSubmit}>
-              <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Group className="mb-3" controlId="formBasicName">
                 <Form.Label>Name</Form.Label>
                 <Form.Control
                   type="text"
@@ -51,17 +69,53 @@ function App() {
               </Form.Group>
 
               <Form.Group className="mb-3" controlId="formBasicPassword">
-                <Form.Label>Phone</Form.Label>
+                <Form.Label>Password</Form.Label>
                 <Form.Control
                   type="text"
-                  placeholder="Phone"
+                  placeholder="Password"
                   onChange={(e) => {
-                    setPhone(e.target.value);
+                    setPass(e.target.value);
                   }}
                 />
               </Form.Group>
+
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Email</Form.Label>
+                <Form.Control
+                  type="email"
+                  placeholder="Neo@Matrix.com"
+                  onChange={(e) => {
+                    setMail(e.target.value);
+                  }}
+                />
+              </Form.Group>
+
+              <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Type</Form.Label>
+                <Form.Select
+                  aria-label="Types"
+                  onChange={(e) => {
+                    setPass(e.target.value);
+                  }}
+                >
+                  <option value="Admin">ADMIN</option>
+                  <option value="Broker">BROKER</option>
+                  <option value="Insured">INSURED</option>
+                </Form.Select>
+              </Form.Group>
+
+              <Form.Group className="mb-3" controlId="formBasicUser">
+                <Form.Label>USER ID for UPDATE OR DELETE</Form.Label>
+                <Form.Control
+                  type="number"
+                  placeholder="0"
+                  onChange={(e) => {
+                    setNum(e.target.value);
+                  }}
+                ></Form.Control>
+              </Form.Group>
               <Button variant="primary" type="submit">
-                USE DATA
+                DO TESTS
               </Button>
             </Form>
           </Row>
